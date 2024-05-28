763f2dd177d390aa4514092c1e009124a2d5e1b4
paul449 <paul_bilbatua@hotmail.com>
2024-05-28 09:01:49 -0500

    seeding thought

---
+++ seeds/ReactionData.js
@@ -0,0 +1,8 @@
+//connecting to mongoose
+const mongoose = require('../config/connection');
+//importing reaction model
+const Reaction = require('../Models/Reaction');
+//Reaction Data
+const ReactionData = [{
+    
+}]
--- seeds/ThoughtData.js
+++ seeds/ThoughtData.js
@@ -2,9 +2,29 @@
 const mongoose = require('../config/connection');
 const thought = require('../Models/Thought');
 const ThoughtData =[{
-
-    
+    thoughtText:'Thoughts are words of our minds',
+    username:'PaulBilbatua89',
 },
 {
+    thoughtText:'Everyone has a plan until they get punched in the face',
+    username:'GeraBiro4675',
 
+},
+{
+    thoughtText:'I want to eat ice cream',
+    username:'Paulbiro5678',
 }]
+//seeding thought model
+const seedThought = async function(){
+    try{
+        await thought.deleteMany({});
+        await thought.create(ThoughtData);
+        console.log('seeding module successfully');
+        const thoughts = await thought.find({});
+        console.log(thoughts);
+    }catch(error){
+        console.error('something unexpectely happened');
+        mongoose.connection.close();
+    }
+}
+seedThought();
