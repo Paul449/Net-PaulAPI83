a598a442753586fdb20eaa331d6e58d2665aa183
paul449 <paul_bilbatua@hotmail.com>
2024-05-28 11:23:49 -0500

    defining routes

--- routes/api/ThoughtRoute.js
+++ routes/api/ThoughtRoute.js
@@ -1,13 +1,35 @@
+const { Router } = require('express');
+
 //defining router from express
 const express = require('express').Router;
 // GET to get all thoughts
+Router.get('/api/thoughts',(req,res)=>{
+
+});
 // GET to get a single thought by its _id
+Router.get('/api/thought/:_id',(req,res)=>{
+
+});
 // POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+Router.post('/api/thought/:_id',(req,res)=>{
+
+});
 // PUT to update a thought by its _id
+Router.put('/api/thought/:_id',(req,res)=>{
+
+});
 // DELETE to remove a thought by its _id
+Router.delete('/api/thought/:_id',(req,res)=>{
+
+});
 
 /*/api/thoughts/:thoughtId/reactions */
 
 // POST to create a reaction stored in a single thought's reactions array field
+Router.post('/api/thoughts/:thoughtId/reactions',(req,res)=>{
 
+});
 // DELETE to pull and remove a reaction by the reaction's reactionId value
+Router.delete('/api/thoughts/:thoughtId/reactions',(req,res)=>{
+
+});
--- routes/api/UserRoute.js
+++ routes/api/UserRoute.js
@@ -1,12 +1,37 @@
+const { Router } = require('express');
+
 //importing express router
 const express = require('express').Router();
 // get api routes
+Router.get('/api/user',(req,res)=>{
+
+});
 // get a single user by its _id
+Router.get('/api/user/:_id',(req,res)=>{
+
+});
 // POST a new user
+Router.post('/api/user/:_id',(req,res)=>{
+
+});
 // PUT to update a user by its _id
+Router.put('/api/user/:_id',(req,res)=>{
+
+});
 // DELETE to remove user by its _id
+Router.delete('/api/user/:_id',(req,res)=>{
+
+});
+
 
 /*/api/users/:userId/friends/:friendId */
 
 // POST to add a new friend to a user's friend list
+Router.post('/api/users/:userId/friends/:friendId',(req,res)=>{
+
+});
+
 // DELETE to remove a friend from a user's friend list
+Router.delete('/api/users/:userId/friends/:friendId',(req,res)=>{
+
+});
