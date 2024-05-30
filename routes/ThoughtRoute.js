704aa964f29474fcb1f4f01cdfc2fd402cd93259
paul449 <paul_bilbatua@hotmail.com>
2024-05-30 13:56:39 -0500

    thoughtroute

--- routes/ThoughtRoute.js
+++ routes/ThoughtRoute.js
@@ -10,9 +10,9 @@ const Router = require('express').Router();
 Router.get('/',async (req,res)=>{
     try{
     let thoughts = await Thought.find({}).populate('thought')
-    res.json(thoughts)
+    res.status(200).json(thoughts)
     }catch(err){
-        res.status('internal server error',err)
+        res.status(500).json({message:'internal server error'},err)
     }
 });
 
@@ -20,49 +20,49 @@ Router.get('/',async (req,res)=>{
 Router.get('/:thoughtId',async(req,res)=>{
     try{
     let thoughts = await Thought.findOne({_id:req.params.thoughtId}).populate('thought')
-    if(thoughts !== true){
+    if(!thoughts){
         return res.status(404).json({ message: 'this thought does not exist' });
     }
     res.json(thoughts);
     }catch(err){
-        res.status(500).json('internal server error',err)
+        res.status(500).json({message:'internal server error'},err)
     }
 });
 // POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-Router.post('/',(req,res)=>{
+Router.post('/',async(req,res)=>{
     try{
-    let newThought = Thought.create(req.body)
+    let newThought = await Thought.create(req.body)
     res.json(newThought)
     }catch(err){
-        res.status(500).json('internal server error',err)
+        res.status(500).json({message:'internal server error'},err)
     }
 });
 // PUT to update a thought by its _id
-Router.put('/:thoughtId',(req,res)=>{
+Router.put('/:thoughtId',async (req,res)=>{
     try{
-    let updateThought = Thought.findByIdAndUpdate(req.body.thoughtId, req.body,{new: true}) //body of thought id
-    if(updateThought !== true){
-        return res.status(404).json('Sorry, this thought does not exist, retry again!!!')
+    let updateThought = await Thought.findByIdAndUpdate(req.body.thoughtId, req.body,{new: true}) //body of thought id
+    if(!updateThought){
+        return res.status(404).json({message:'Sorry, this thought does not exist, retry again!!!'})
     }
     res.json(updateThought)
     }catch(err){
-        res.status(500).json('internal server error',err)
+        res.status(500).json({message:'internal server error'},err)
     }
 });
 // DELETE to remove a thought by its _id
-Router.delete('/:thoughtId',(req,res)=>{
+Router.delete('/:thoughtId',async (req,res)=>{
     try{
-    let deleteThought = Thought.findByIdAndDelete({_id:req.params.thoughtId})
+    let deleteThought = await Thought.findByIdAndDelete({_id:req.params.thoughtId})
     res.json(deleteThought)
     }catch(err){
-        res.status(500).json('internal server error:',err)
+        res.status(500).json({message:'internal server error:'},err)
     }
 });
 
 /*/api/thoughts/:thoughtId/reactions */
 
 // POST to create a reaction stored in a single thought's reactions array field
-Router.post('/:thoughtId/reactions/:reactionsId',async(req,res)=>{
+Router.post('/:thoughtId/reactions',async(req,res)=>{
     try{
         let updateThought = await Thought.findOneAndUpdate(req.params.thoughtId,{$push:{reactions:req.body}},{new:true, useFindAndModify:false})
         if(!updateThought){
@@ -74,9 +74,9 @@ Router.post('/:thoughtId/reactions/:reactionsId',async(req,res)=>{
     }
 });
 // DELETE to pull and remove a reaction by the reaction's reactionId value
-Router.delete('/:thoughtId/reactions/:reactionsId',(req,res)=>{
+Router.delete('/:thoughtId/reactions/:reactionsId',async(req,res)=>{
     try{
-        let removeReaction = Thought.findOneAndUpdate({_id:req.params.thoughtId},{$pull:{reactions:req.body}},{new:true, useFindAndModify:false})
+        let removeReaction = await Thought.findOneAndUpdate({_id:req.params.thoughtId},{$pull:{reactions:req.body}},{new:true, useFindAndModify:false})
         if(!removeReaction){
             res.status(404).json('reaction not found!!!')
         }
